b178c7be56b5d7d2761af3f80e54e87d6f522fe8
Jordan Bravo <jordan@bravo.cc>
2024-04-19 14:54:17 -0400

    add plugin: rainbow-delimiters

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -44,3 +44,38 @@ require("lazy").setup({
     },
   },
 })
+
+-- ------------------------------
+-- Config for the plugin rainbow-delimiters
+-- TODO: move this config into more appropriate location
+
+-- This module contains a number of default definitions
+local rainbow_delimiters = require("rainbow-delimiters")
+
+---@type rainbow_delimiters.config
+vim.g.rainbow_delimiters = {
+  strategy = {
+    [""] = rainbow_delimiters.strategy["global"],
+    commonlisp = rainbow_delimiters.strategy["local"],
+  },
+  query = {
+    [""] = "rainbow-delimiters",
+    lua = "rainbow-blocks",
+  },
+  priority = {
+    [""] = 110,
+    lua = 210,
+  },
+  highlight = {
+    "RainbowDelimiterRed",
+    "RainbowDelimiterYellow",
+    "RainbowDelimiterBlue",
+    "RainbowDelimiterOrange",
+    "RainbowDelimiterGreen",
+    "RainbowDelimiterViolet",
+    "RainbowDelimiterCyan",
+  },
+  blacklist = { "c", "cpp" },
+}
+
+-- -------------------------------
